0106d3c06b0ddeb3bda541f8a66075f481fe1078
Tanisha0517 <tanisharambadia@gmail.com>
2026-08-06 16:24:57 +0530

    Done

--- JDBC_Connection/src/com/rays/jdbc/preparedstmt/MarkSheetModel.java
+++ JDBC_Connection/src/com/rays/jdbc/preparedstmt/MarkSheetModel.java
@@ -2,6 +2,9 @@ package com.rays.jdbc.preparedstmt;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.rays.jdbc.util.JDBCDataSource;
 
@@ -38,73 +41,108 @@ public class MarkSheetModel {
 
 		}
 	}
-		
+
 //		-------------------------------------------------------------------------------------
-		
-		
-		public void update(MarkSheetBean bean) throws Exception {
 
-			Connection conn = null;
+	public void update(MarkSheetBean bean) throws Exception {
 
-			try {
+		Connection conn = null;
 
-				conn = JDBCDataSource.getConnection();
+		try {
 
-				conn.setAutoCommit(false);
+			conn = JDBCDataSource.getConnection();
 
-				PreparedStatement pstmt = conn.prepareStatement(
-						"update marksheet set rollno = ?, name = ?, phy = ?, chem = ?, maths = ? where id = ?");
+			conn.setAutoCommit(false);
 
-				pstmt.setInt(1, bean.getRollno());
-				pstmt.setString(2, bean.getName());
-				pstmt.setInt(3, bean.getPhy());
-				pstmt.setInt(4, bean.getChem());
-				pstmt.setInt(5, bean.getMaths());
-				pstmt.setInt(6, bean.getId());
+			PreparedStatement pstmt = conn.prepareStatement(
+					"update marksheet set rollno = ?, name = ?, phy = ?, chem = ?, maths = ? where id = ?");
 
-				int i = pstmt.executeUpdate();
+			pstmt.setInt(1, bean.getRollno());
+			pstmt.setString(2, bean.getName());
+			pstmt.setInt(3, bean.getPhy());
+			pstmt.setInt(4, bean.getChem());
+			pstmt.setInt(5, bean.getMaths());
+			pstmt.setInt(6, bean.getId());
 
-				conn.commit();
+			int i = pstmt.executeUpdate();
 
-				System.out.println("record updated successfully: " + i);
+			conn.commit();
 
-			} catch (Exception e) {
-				e.printStackTrace();
-				conn.rollback();
-			} finally {
-				conn.close();
-			}
+			System.out.println("record updated successfully: " + i);
 
+		} catch (Exception e) {
+			e.printStackTrace();
+			conn.rollback();
+		} finally {
+			conn.close();
 		}
+
+	}
 //		-------------------------------------------------------------------------------------
 
-		public void delete(int id) throws Exception {
+	public void delete(int id) throws Exception {
 
-			Connection conn = null;
+		Connection conn = null;
 
-			try {
+		try {
 
-				conn = JDBCDataSource.getConnection();
+			conn = JDBCDataSource.getConnection();
 
-				conn.setAutoCommit(false);
+			conn.setAutoCommit(false);
 
-				PreparedStatement pstmt = conn.prepareStatement("delete from marksheet where id = ?");
+			PreparedStatement pstmt = conn.prepareStatement("delete from marksheet where id = ?");
 
-				pstmt.setInt(1, id);
+			pstmt.setInt(1, id);
 
-				int i = pstmt.executeUpdate();
+			int i = pstmt.executeUpdate();
 
-				conn.commit();
+			conn.commit();
 
-				System.out.println("record delete successfully: " + i);
+			System.out.println("record delete successfully: " + i);
 
-			} catch (Exception e) {
-				e.printStackTrace();
-				conn.rollback();
-			} finally {
-				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			conn.rollback();
+		} finally {
+			conn.close();
+		}
+
+	}
+
+//		--------------------------------
+
+	public List<MarkSheetBean> search(MarkSheetBean bean, int pageNo, int pageSize) throws Exception {
+
+		Connection conn = null;
+		List<MarkSheetBean> list = new ArrayList<MarkSheetBean>();
+		try {
+			conn = JDBCDataSource.getConnection();
+
+			
+
+			PreparedStatement pstmt = conn.prepareStatement("select *, (phy+chem+maths) as total, ((phy+chem+maths)/3) as percentage, if(phy >= 33 and chem >= 33 and maths >= 33, 'pass', 'fail') as result from marksheet order by total desc;");
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				bean = new MarkSheetBean();
+				bean.setId(rs.getInt("id"));
+				bean.setRollno(rs.getInt("rollno"));
+				bean.setName(rs.getString("name"));
+				bean.setPhy(rs.getInt("phy"));
+				bean.setChem(rs.getInt("chem"));
+				bean.setMaths(rs.getInt("maths"));
+				
+				list.add(bean);
 			}
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn.close();
+		}
+
+		return list;
 
 	}
 
--- JDBC_Connection/src/com/rays/jdbc/preparedstmt/TestMarkSheetModel.java
+++ JDBC_Connection/src/com/rays/jdbc/preparedstmt/TestMarkSheetModel.java
@@ -1,9 +1,13 @@
 package com.rays.jdbc.preparedstmt;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class TestMarkSheetModel {
 	public static void main(String[] args) throws Exception {
 
-		testAdd();
+//		testAdd();
+		testSearch();
 
 	}
 
@@ -19,9 +23,43 @@ public class TestMarkSheetModel {
 		bean.setPhy(90);
 		bean.setChem(91);
 		bean.setMaths(85);
-		
-		
 
 		ms.add(bean);
 	}
+
+//	---------------------Search-----------------------
+
+	public static void testSearch() throws Exception {
+
+		MarkSheetBean bean = new MarkSheetBean();
+		MarkSheetModel ms = new MarkSheetModel();
+		// bean.setFirstName("v");
+		List<MarkSheetBean> list = ms.search(bean, 1, 10);
+
+		Iterator<MarkSheetBean> it = list.iterator();
+
+		while (it.hasNext()) {
+			bean = it.next();
+			int total = (bean.getPhy()+bean.getMaths()+bean.getChem());
+			double percentage = ((bean.getPhy()+bean.getMaths()+bean.getChem())/3);
+			
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getRollno());
+			System.out.print("\t" + bean.getName());
+			System.out.print("\t" + bean.getPhy());
+			System.out.print("\t" + bean.getChem());
+			System.out.print("\t" + bean.getMaths());
+			System.out.print("\t" + total);
+			System.out.print("\t" +percentage);
+			
+			
+			if (bean.getPhy() < 33 || bean.getChem() < 33 || bean.getMaths() < 33) {
+				System.out.println("\t" + "Fail");
+			} else {
+				System.out.println("\t" + "Pass");
+			}
+		}
+
+	}
+
 }
